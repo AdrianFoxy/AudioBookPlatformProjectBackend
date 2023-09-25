586a88b2ee2c8b2125dfd830ec8d20a4b5359cf2
AdrianFoxy <drugakadrian@gmail.com>
2023-09-25 08:31:05 +0300

    CleaningUpThePrograms.cs

---
+++ Exntensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,50 @@
+﻿using ABP_Backend.Data.DB;
+using ABP_Backend.Data.Interfraces;
+using ABP_Backend.Data.Repositories;
+using ABP_Backend.Errors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ABP_Backend.Exntensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services
+            , IConfiguration config)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddDbContext<AppDBContext>(options =>
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IAudioBookRepository, AudioBookRepository>();
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            // ErrorsHandler
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var errors = actionContext.ModelState
+                    .Where(e => e.Value.Errors.Count > 0)
+                    .SelectMany(x => x.Value.Errors)
+                    .Select(x => x.ErrorMessage).ToArray();
+
+                    var errorResponse = new ApiValidationErrorResponse
+                    {
+                        Errors = errors
+                    };
+
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
+
+            return services;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,46 +1,13 @@
 using ABP_Backend.Data.DB;
-using ABP_Backend.Data.Interfraces;
-using ABP_Backend.Data.Repositories;
-using ABP_Backend.Errors;
+using ABP_Backend.Exntensions;
 using ABP_Backend.Middleware;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<AppDBContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddScoped<IAudioBookRepository, AudioBookRepository>();
-
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-// ErrorsHandler
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.InvalidModelStateResponseFactory = actionContext =>
-    {
-        var errors = actionContext.ModelState
-        .Where(e => e.Value.Errors.Count > 0)
-        .SelectMany(x => x.Value.Errors)
-        .Select(x => x.ErrorMessage).ToArray();
-
-        var errorResponse = new ApiValidationErrorResponse
-        {
-            Errors = errors
-        };
-
-        return new BadRequestObjectResult(errorResponse);
-    };
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
