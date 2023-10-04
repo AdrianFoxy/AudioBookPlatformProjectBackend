5e9d54e2555b2886f85f35601aa6d0acbab8a7cd
AdrianFoxy <drugakadrian@gmail.com>
2023-10-04 11:22:10 +0300

    Issue with genre filterig Fixed

--- Re_ABP_Backend/Data/Specification/SpecClasses/AudioBook/LibraryAudioBookSpecification.cs
+++ Re_ABP_Backend/Data/Specification/SpecClasses/AudioBook/LibraryAudioBookSpecification.cs
@@ -7,14 +7,15 @@ namespace Re_ABP_Backend.Data.Specification.SpecClasses.AudioBooks
         public LibraryAudioBookSpecification(ABSpecParams abParams)
             : base(x =>
                 (string.IsNullOrEmpty(abParams.Search) || x.Name.ToLower().Contains(abParams.Search)) &&
+
                 (abParams.AuthorIds == null || abParams.AuthorIds.Count == 0 || x.Author.Any(x => abParams.AuthorIds.Contains(x.Id))) &&
-                (abParams.GenreIds == null || abParams.GenreIds.Count == 0 || x.Genre.Any(x => abParams.GenreIds.Contains(x.Id))) &&
                 (abParams.GenreIds == null || abParams.GenreIds.Count == 0 || x.Genre.Any(x => abParams.GenreIds.Contains(x.Id))) &&
                 (abParams.BookSeriesIds == null || abParams.BookSeriesIds.Count == 0 || abParams.BookSeriesIds.Contains(x.BookSeriesId)) &&
                 (abParams.BookLanguageIds == null || abParams.BookLanguageIds.Count == 0 || abParams.BookLanguageIds.Contains(x.BookLanguageId)) &&
                 (abParams.NarratorIds == null || abParams.NarratorIds.Count == 0 || abParams.NarratorIds.Contains(x.NarratorId)) &&
+
                 (abParams.ExceptAuthorIds == null || abParams.ExceptAuthorIds.Count == 0 || !x.Author.Any(a => abParams.ExceptAuthorIds.Contains(a.Id))) &&
-                (abParams.ExceptGenreIds == null || abParams.ExceptGenreIds.Count == 0 || !x.Author.Any(a => abParams.ExceptGenreIds.Contains(a.Id))) &&
+                (abParams.ExceptGenreIds == null || abParams.ExceptGenreIds.Count == 0 || !x.Genre.Any(a => abParams.ExceptGenreIds.Contains(a.Id))) &&
                 (abParams.ExceptBookSeriesIds == null || !abParams.ExceptBookSeriesIds.Contains(x.BookSeriesId)) &&
                 (abParams.ExceptBookLanguageIds == null || !abParams.ExceptBookLanguageIds.Contains(x.BookLanguageId)) &&
                 (abParams.ExceptNarratorIds == null || !abParams.ExceptNarratorIds.Contains(x.NarratorId))
