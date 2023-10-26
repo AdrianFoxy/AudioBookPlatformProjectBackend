9354c2fa718e354207cc9e38c1bce72ea173808c
AdrianFoxy <drugakadrian@gmail.com>
2023-10-26 07:47:25 +0300

    Update_Selection Model&DB

--- Re_ABP_Backend/Data/DB/Config/BookSelectionConfiguration.cs
+++ Re_ABP_Backend/Data/DB/Config/BookSelectionConfiguration.cs
@@ -9,8 +9,15 @@ namespace Re_ABP_Backend.Data.DB.Config
         public void Configure(EntityTypeBuilder<BookSelection> builder)
         {
             builder.Property(p => p.Name).IsRequired().HasMaxLength(256);
+            builder.Property(p => p.EnName).IsRequired().HasMaxLength(256);
             builder.Property(p => p.Description).IsRequired().HasColumnType("text");
+            builder.Property(p => p.EnDescription).IsRequired().HasColumnType("text");
             builder.Property(p => p.ImageUrl).IsRequired().HasColumnType("text");
+            builder.Property(p => p.ViewCount)
+                   .IsRequired()
+                   .HasColumnType("int")
+                   .HasDefaultValue(0);
+
         }
     }
 }
---
+++ Re_ABP_Backend/Migrations/20231026044401_selection_update.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Re_ABP_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class selection_update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EnDescription",
+                table: "BookSelection",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EnName",
+                table: "BookSelection",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ViewCount",
+                table: "BookSelection",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnDescription",
+                table: "BookSelection");
+
+            migrationBuilder.DropColumn(
+                name: "EnName",
+                table: "BookSelection");
+
+            migrationBuilder.DropColumn(
+                name: "ViewCount",
+                table: "BookSelection");
+        }
+    }
+}
