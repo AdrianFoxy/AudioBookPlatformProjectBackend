a46758720c7f4b7029bcfb3a8faa1e27671ac4a2
AdrianFoxy <drugakadrian@gmail.com>
2023-11-02 09:26:21 +0200

    rework selection

--- Re_ABP_Backend/Data/DB/Config/BookSelectionConfiguration.cs
+++ Re_ABP_Backend/Data/DB/Config/BookSelectionConfiguration.cs
@@ -13,10 +13,6 @@ namespace Re_ABP_Backend.Data.DB.Config
             builder.Property(p => p.Description).IsRequired().HasColumnType("text");
             builder.Property(p => p.EnDescription).IsRequired().HasColumnType("text");
             builder.Property(p => p.ImageUrl).IsRequired().HasColumnType("text");
-            builder.Property(p => p.ViewCount)
-                   .IsRequired()
-                   .HasColumnType("int")
-                   .HasDefaultValue(0);
 
         }
     }
