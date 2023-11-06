e920531c5e54b5cdcfc9a18f70f3287dd6591bd5
AdrianFoxy <drugakadrian@gmail.com>
2023-11-06 08:01:12 +0200

    ReviewAdded

---
+++ Re_ABP_Backend/Data/Entities/Review.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Re_ABP_Backend.Data.Entities.Base;
+using Re_ABP_Backend.Data.Entities.Identity;
+
+namespace Re_ABP_Backend.Data.Entities
+{
+    public class Review : BaseEntity
+    {
+        public string ReviewText { get; set; }
+        public int Rating { get; set; }
+        public int AudioBookId { get; set; }
+        [JsonIgnore]
+        public AudioBook AudioBook { get; set; }
+        public int UserId { get; set; }
+
+        [JsonIgnore]
+        public User User { get; set; }
+    }
+}
