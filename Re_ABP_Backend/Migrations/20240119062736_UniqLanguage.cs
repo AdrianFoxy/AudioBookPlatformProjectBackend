771fee047a771950c97c775f96ad1bceca81f9aa
AdrianFoxy <drugakadrian@gmail.com>
2024-01-19 08:32:18 +0200

    UniqSQL for bookLanguage

---
+++ Re_ABP_Backend/Migrations/20240119062736_UniqLanguage.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Re_ABP_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqLanguage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_BookLanguage_EnName",
+                table: "BookLanguage",
+                column: "EnName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookLanguage_Name",
+                table: "BookLanguage",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BookLanguage_EnName",
+                table: "BookLanguage");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BookLanguage_Name",
+                table: "BookLanguage");
+        }
+    }
+}
