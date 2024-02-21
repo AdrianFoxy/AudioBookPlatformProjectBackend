c9a35dacbf150ded68170348eb5ce0fbe3ac91b9
AdrianFoxy <drugakadrian@gmail.com>
2024-02-21 08:43:36 +0200

    validation

--- Re_ABP_Backend/Data/Dtos/AdminManagmentDtos/AudioBooksDtos/AddAudioBookDto.cs
+++ Re_ABP_Backend/Data/Dtos/AdminManagmentDtos/AudioBooksDtos/AddAudioBookDto.cs
@@ -1,19 +1,31 @@
 ﻿using Re_ABP_Backend.Data.Dtos.AdminManagmentDtos.AudioBooksDtos.AudioFiles;
 using Re_ABP_Backend.Data.Helpers.Validation;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Re_ABP_Backend.Data.Dtos.AdminManagmentDtos.AudioBooksDtos
 {
     public class AddAudioBookDto
     {
+        [Required]
+        [MaxLength(256, ErrorMessage = "Max lenght is 256.")]
         public string Name { get; set; }
+        [Required]
+        [MaxLength(800, ErrorMessage = "Max lenght is 800.")]
         public string Description { get; set; }
+        [Required]
         public int BookLanguageId { get; set; }
+        [Required]
         public int NarratorId { get; set; }
+        [Required]
         public int BookSeriesId { get; set; }
+        [Required]
         public int OrderInSeries { get; set; }
-        public List<int> AuthorsIds { get; set; }   
+        [Required]
+        public List<int> AuthorsIds { get; set; }
+        [Required]
         public List<int> GenresIds { get; set; }
+        [Required]
         public string AudioFiles { get; set; }
 
         [NotMapped]
